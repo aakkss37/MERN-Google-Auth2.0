74f0e773ee06e1e7de56c402b3ae253c070e2688
amar kumar <amarkumar.sharma.124@gmail.com>
2023-02-27 19:30:01 +0530

    whole login handled

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css';
 import { LoginSocialGoogle } from 'reactjs-social-login';
 import { GoogleLoginButton } from 'react-social-login-buttons';
+const client_id = process.env.REACT_APP_CLIENT_ID
+
+
 
 function App () {
+	const [user, setUser] = useState();
 	const handleLoginResponce = (data) => {
-		console.log(data)
+		setUser({
+			name: data.name,
+			email: data.email,
+			img: data.picture
+		})
 	}
-
 
 	return (
 		<div className="App">
 			<header className="App-header">
-				<LoginSocialGoogle
-					client_id={""}
-					scope="openid profile email"
-					discoveryDocs="claims_supported"
-					access_type="offline"
-					onResolve={({ provider, data }) => {
-						handleLoginResponce(data)
-					}}
-					onReject={err => {
-						console.log(err);
-					}}
-					hosted_domain="kahedu.edu.in"
-				>
-					<GoogleLoginButton />
-				</LoginSocialGoogle>
+				{
+					!user ?
+						<LoginSocialGoogle
+							client_id={client_id}
+							scope="openid profile email"
+							discoveryDocs="claims_supported"
+							access_type="offline"
+							onResolve={({ provider, data }) => {
+								handleLoginResponce(data)
+							}}
+							onReject={err => {
+								console.log(err);
+							}}
+							hosted_domain="kahedu.edu.in"
+						>
+							<GoogleLoginButton />
+						</LoginSocialGoogle>
+						:
+						<div>
+							<img src={user.img} alt='proPic' />
+							<h2>Welcome {user.name}</h2>
+							<span>{user.email}</span>
+							<br />
+							<br />
+							<button onClick={() => { setUser() }}>Logout</button>
+						</div>
+				}
+
 			</header>
 		</div>
 	);
