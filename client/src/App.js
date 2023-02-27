292155f10be0bb3e3128f79b08513683c1396f60
amar kumar <amarkumar.sharma.124@gmail.com>
2023-02-27 18:05:20 +0530

    login button created

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,35 @@
+import React from 'react'
 import './App.css';
+import { LoginSocialGoogle } from 'reactjs-social-login';
+import { GoogleLoginButton } from 'react-social-login-buttons';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
-  );
+function App () {
+	const handleLoginResponce = (data) => {
+		console.log(data)
+	}
+
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<LoginSocialGoogle
+					client_id={""}
+					scope="openid profile email"
+					discoveryDocs="claims_supported"
+					access_type="offline"
+					onResolve={({ provider, data }) => {
+						handleLoginResponce(data)
+					}}
+					onReject={err => {
+						console.log(err);
+					}}
+					hosted_domain="kahedu.edu.in"
+				>
+					<GoogleLoginButton />
+				</LoginSocialGoogle>
+			</header>
+		</div>
+	);
 }
 
 export default App;
