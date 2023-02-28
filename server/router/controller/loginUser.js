8ac0042b943fce78041b3af36477fe0b4e85ce6c
amar kumar <amarkumar.sharma.124@gmail.com>
2023-02-28 15:36:46 +0530

    google access token varified from backend

--- server/router/controller/loginUser.js
+++ server/router/controller/loginUser.js
@@ -1,7 +1,19 @@
-
+import axios from 'axios';
 
 export const loginUser = async(req, resp)=> {
 	// console.log("body ===>>> ",req.body);
 	const access_token = req.body.access_token
-	
+
+	try {
+		const responce = await axios.get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${access_token}`,{
+			headers: {
+				"Authorization": `Bearer ${access_token}`,
+				"Accept": 'application/json'
+			}
+		})
+		console.log("incomming data==> ",responce.data)
+	} catch (error) {
+		console.log(error)
+	}
+
 }
